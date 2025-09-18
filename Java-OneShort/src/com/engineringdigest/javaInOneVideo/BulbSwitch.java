91f6ce115427daf5cd122d3962eda8b4e34cc3cb
Christeen Paul <chrispaul1311@gmail.com>
2025-09-18 20:48:43 +0530

    Jva-DSA

---
+++ Java-OneShort/src/com/engineringdigest/javaInOneVideo/BulbSwitch.java
@@ -0,0 +1,20 @@
+package com.engineringdigest.javaInOneVideo;
+
+public class BulbSwitch {
+    public static void main(String[] args) {
+        int n=3;
+
+        int count=0;
+
+        int i=1;
+
+        while(i*i<=n){
+            count++;
+            i++;
+        }
+
+        System.out.println(count);
+
+
+    }
+}
